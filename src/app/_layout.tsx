698075cad8144d39d3582e39f35f2dd9ce70235e
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2024-12-14 21:26:54 +1100

    chore: add tailwind

---
+++ src/app/_layout.tsx
@@ -0,0 +1,33 @@
+import { colors } from "@/styles/theme";
+import { Stack } from "expo-router";
+import "../styles/global.css";
+
+import {
+  useFonts,
+  Rubik_600SemiBold,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_700Bold,
+} from "@expo-google-fonts/rubik";
+
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.gray[100] },
+      }}
+    />
+  );
+}
