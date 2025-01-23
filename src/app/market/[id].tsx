f234463fbf9809ae56c3e88fa30b04675899ac44
Danilo Gomes <51554360+dantls@users.noreply.github.com>
2025-01-23 23:30:57 +1100

    feat: add camera on market page

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,12 +1,17 @@
-import { View, Text, Alert } from "react-native";
+import { View, Text, Alert, Modal } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { api } from "@/services/api";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { useCameraPermissions, CameraView } from "expo-camera";
+
 
 export default function Market() {
   const params = useLocalSearchParams<{ id: string }>();
   const [coupon, setCoupon] = useState<string | null>();
-  const [couponIsFetching, setCouponIsFetching] = useState(false);
+  const [couponIsFetching, setCouponIsFetching] = useState(false); 
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
+  const [_, requestPermission] = useCameraPermissions();
+  const qrLock = useRef(false);
 
   async function fetchMarket() {
     try {
@@ -30,6 +35,40 @@ export default function Market() {
     }
   }
 
+  function handleUseCoupon(id: string) {
+    setIsVisibleCameraModal(false);
+    Alert.alert(
+      "Coupon",
+      "Redeemed coupon cannot be reused. Do you really want to redeem the coupon?",
+      [
+        {
+          style: "cancel",
+          text: "No",
+        },
+        {
+          text: "Yes",
+          onPress: () => getCoupon(id),
+        },
+      ]
+    );
+  }
+
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission();
+
+      if (!granted) {
+        return Alert.alert("Camera", "We need to access the camera");
+      }
+
+      qrLock.current = false;
+      setIsVisibleCameraModal(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Camera", "It doesn`t possible to access the camera");
+    }
+  }
+
   useEffect(() => {
     fetchMarket();
   }, [params.id]);
@@ -37,6 +76,18 @@ export default function Market() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text>{params.id}</Text>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <CameraView
+          style={{ flex: 1 }}
+          facing="back"
+          onBarcodeScanned={({ data }) => {
+            if (data && !qrLock.current) {
+              qrLock.current = true;
+              setTimeout(() => handleUseCoupon(data), 500);
+            }
+          }}
+        />
     </View>
   );
 }
